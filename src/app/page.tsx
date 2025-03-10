2f41db7345e624c999702261c79a5e369ece461c
sakshipandey2601 <pandeysakshi2601@gmail.com>
2025-03-10 18:08:33 +0530

    feat: responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ import { Hero } from "@/components/pages/Hero";
 export default function Home() {
 	return (
 		<main>
-			{/* <Navbar /> */}
 			<Hero />
-			{/* <Products /> */}
-			{/* <Products /> */}
 		</main>
 	);
 }
