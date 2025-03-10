f4a9361e966a85f3130a7b2491719c2be5b52d98
sakshipandey2601 <pandeysakshi2601@gmail.com>
2025-03-10 13:29:25 +0530

    feat: mobile menu

--- src/components/atoms/ContactBtn.tsx
+++ src/components/atoms/ContactBtn.tsx
@@ -1,3 +1,7 @@
-export function ContactBtn() {
-	return <button className="bg-lime-green  rounded-4xl font-semibold py-4 px-6 text-sm"> Contact us</button>;
+interface contactProps {
+	className?: string;
+}
+
+export function ContactBtn({ className }: contactProps) {
+	return <button className={` ${className} bg-lime-green  rounded-4xl font-semibold py-4 px-6 text-sm  `}> Contact us</button>;
 }
--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -4,14 +4,16 @@ import { Logo } from "../atoms/Logo";
 import { navItems } from "@/data/data";
 import { ContactBtn } from "../atoms/ContactBtn";
 import React from "react";
+import { Menu, X } from "lucide-react";
+import MoblieMenu from "../atoms/MoblieMenu";
 
 export default function Navbar() {
 	// const [ActiveComponent, setActiveComponent] = useState<FC | null>(null);
 	const [ActiveComponent, setActiveComponent] = useState(" ");
-
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const activeItem = navItems.find((item) => item.name === ActiveComponent);
 	// console.log(activeItem);
-
+	const menuItems = ["Products", "Solutions", "Partners", "Developers", "Blogs"];
 	return (
 		<>
 			<nav className="hidden lg:flex justify-between items-center py-[11px] px-[60px]  bg-white fixed top-0 left-0 w-full z-10">
@@ -33,15 +35,20 @@ export default function Navbar() {
 					</li>
 				</ul>
 			</nav>
-			<div className=" absolute w-full top-[74px] z-0 ">
-				{/* display selected or hovered navitem component */}
-				{/* {ActiveComponent && <ActiveComponent />} */}
-				{activeItem && <activeItem.page />}
-			</div>
-			<nav className="flex justify-between items-center lg:hidden px-3.5 py-4 fixed top-0 left-0 w-full bg-white-100">
+			<div className="hidden md:block absolute w-full top-[74px] z-0 ">{activeItem && <activeItem.page />}</div>
+			{/* Moblie Nav */}
+			<nav className="flex justify-between items-center lg:hidden px-4 py-4 fixed top-0 left-0 w-full bg-white-100">
 				<Logo width={94} height={24} />
-				<span className="w-4 font-extrabold text-title text-2xl">&#9776;</span>
+				<span className="w-4 font-extrabold text-title text-2xl" onClick={() => setIsOpen(!isOpen)}>
+					{isOpen ? <X size={24} /> : <Menu size={24} />}
+				</span>
 			</nav>
+
+			{/* Mobile Menu */}
+			<div className={`absolute w-full top-[56px] z-10 bg-white transition-all duration-300 ${isOpen ? "block" : "hidden"} p-4`}>
+				<MoblieMenu items={menuItems} className="h-screen" />
+				<ContactBtn className="mt-5 w-full" />
+			</div>
 		</>
 	);
 }
