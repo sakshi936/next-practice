9b889a551192d7c8ef472c3d3d199d54c850fe23
sakshipandey2601 <pandeysakshi2601@gmail.com>
2025-03-10 20:55:58 +0530

    fix: grid cols in design

--- src/components/organisms/solutions/ByUseCase.tsx
+++ src/components/organisms/solutions/ByUseCase.tsx
@@ -1,7 +1,7 @@
 import { byUseCase } from "@/data/data";
 import Features from "../Feaures";
 function ByUseCase() {
-	return <Features features={byUseCase} className="gap-x-7 gap-y-7" category="By use Case" />;
+	return <Features features={byUseCase} className="grid-cols-2 gap-x-7 gap-y-7" category="By use Case" />;
 }
 
 export default ByUseCase;
