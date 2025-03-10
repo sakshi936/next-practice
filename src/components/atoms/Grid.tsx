c58e949e52da2b6ad5b2ca7f7de3fd2b48c4f8b7
sakshipandey2601 <pandeysakshi2601@gmail.com>
2025-03-10 19:55:53 +0530

    fix: icon undefined error

--- src/components/atoms/Grid.tsx
+++ src/components/atoms/Grid.tsx
@@ -11,7 +11,7 @@ function Grid({ features, className }: FeaturesProps) {
 		<div className={`grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 ${className} bg-white`}>
 			{features.map((feature, index) => (
 				<div key={index} className="flex items-start gap-3 min-h-14">
-					<Image src={feature.icon} alt={feature.title} width={40} height={40} />
+					{feature.icon && <Image src={feature.icon} alt={feature.title} width={40} height={40} />}
 					<div>
 						<h3 className="font-semibold text-base text-title">{feature.title}</h3>
 						<p className="text-xs text-content-text">{feature.desc}</p>
